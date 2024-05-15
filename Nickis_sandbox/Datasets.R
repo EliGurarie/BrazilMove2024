cf60b3dca7fe0260604056d876a5b0706b07bf40
barb3800 <nicole.ann.barbour@gmail.com>
2024-05-15 14:06:48 -0400

    create example data, create lecture 1

--- Nickis_sandbox/Datasets.R
+++ Nickis_sandbox/Datasets.R
@@ -4,12 +4,34 @@
 
 # Hebblewhite & Merrill 2007
 # DOI: https://doi.org/10.1007/s00442-007-0661-y
-# N= 14 VHF, N=16 GPS collars
+# N= 14 VHF, N=16 GPS collared wolves
 
 # UTM zone North: 11N
 
+elk <- read.csv("./data/Elk_data.csv")
+wolf <- read.csv("./data/Wolf_data.csv")
+
+length(unique(elk$individual.local.identifier)) # 132 individuals
+
+length(unique(subset(elk, sensor.type=="gps")$individual.local.identifier)) # 31 gps ids
+
+length(unique(wolf$individual.local.identifier)) # 15 individuals
+
+length(unique(subset(wolf, sensor.type=="gps")$individual.local.identifier)) # 15 gps ids
+
+wolf_gps <- subset(wolf, sensor.type=="gps")
+elk_gps <-subset(elk, sensor.type=="gps")
+
+# save data
+save(wolf_gps, file="./data/wolf_gps.rda")
+save(elk_gps, file="./data/elk_gps.rda")
 
 # -----------------------------------------------------------------------
 
 # Mountain tapir, Colombia (N=3)
 # The Nature Conservancy Colombia
+
+tapir <- read.csv("./data/Mountaintapir_Colombia.csv")
+
+# save
+save(tapir, file="./data/tapir.rda")
