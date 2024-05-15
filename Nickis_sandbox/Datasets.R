3a1cab68922ccb2e66f52fff894c3e2c923b33e0
barb3800 <nicole.ann.barbour@gmail.com>
2024-05-15 09:10:06 -0400

    make nicki's sandbox, add data to data folder

---
+++ Nickis_sandbox/Datasets.R
@@ -0,0 +1,15 @@
+# Hebblewhite et al 2008
+# DOI: https://doi.org/10.1890/06-1708.1
+# migrant and resident elk (Cervus elaphus), all females (N=119)
+
+# Hebblewhite & Merrill 2007
+# DOI: https://doi.org/10.1007/s00442-007-0661-y
+# N= 14 VHF, N=16 GPS collars
+
+# UTM zone North: 11N
+
+
+# -----------------------------------------------------------------------
+
+# Mountain tapir, Colombia (N=3)
+# The Nature Conservancy Colombia
